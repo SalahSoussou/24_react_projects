b1f5c351238926e02bbbf388208a046ae4135c7e
SalahSoussou <kaka79702@gmail.com>
2024-02-13 11:54:32 +0100

    ok

--- src/components/accordian/Accordian.jsx
+++ src/components/accordian/Accordian.jsx
@@ -1,3 +1,26 @@
+// single select
+
+import { useState } from "react";
+import data from "./data";
+
 export default function Accordian() {
-  return <div>Accordian</div>;
+  const [selected, setSelected] = useState(null);
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((e) => (
+            <div className="item">
+              <div className="titel">
+                <h3>{e.question}</h3>
+                <span>+</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>no data</div>
+        )}
+      </div>
+    </div>
+  );
 }
