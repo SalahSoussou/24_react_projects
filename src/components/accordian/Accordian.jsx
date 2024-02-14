b1c7e2726ce4c3b823b414a882750e7c16e65e0d
SalahSoussou <kaka79702@gmail.com>
2024-02-14 11:46:44 +0100

    ok

--- src/components/accordian/Accordian.jsx
+++ src/components/accordian/Accordian.jsx
@@ -5,15 +5,22 @@ import data from "./data";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+  function handleSingleSeliction(currentId) {
+    console.log(currentId);
+    setSelected(currentId === selected ? null : currentId);
+  }
   return (
     <div className="wrapper">
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((e) => (
             <div className="item">
-              <div className="titel">
+              <div
+                onClick={() => handleSingleSeliction(e.id)}
+                className="titel"
+              >
                 <h3>{e.question}</h3>
-                <span>+</span>
+                <div>{selected === e.id ? e.answer : <span>+</span>}</div>
               </div>
             </div>
           ))
